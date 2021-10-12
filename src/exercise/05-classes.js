fffe2a48cf94469cbf60c21d6f9d0bd89437e29f
Kristi Achilleos <krispy2009@gmail.com>
2021-10-12 17:52:48 +0300

    refactor classes to hooks

--- src/exercise/05-classes.js
+++ src/exercise/05-classes.js
@@ -8,7 +8,30 @@ import VanillaTilt from 'vanilla-tilt'
 // If you'd rather practice refactoring a class component to a function
 // component with hooks, then go ahead and do this exercise.
 
-class Tilt extends React.Component {
+function Tilt({children}) {
+  const myRef = React.useRef()
+
+  React.useEffect(() => {
+    const tiltNode = myRef.current
+    const vanillaTiltOptions = {
+      max: 25,
+      speed: 400,
+      glare: true,
+      'max-glare': 0.5,
+    }
+    VanillaTilt.init(tiltNode, vanillaTiltOptions)
+
+    return () => tiltNode.VanillaTilt.destroy()
+  }, [])
+
+  return (
+    <div ref={myRef} className="tilt-root">
+      <div className="tilt-child">{children}</div>
+    </div>
+  )
+}
+
+class TiltClasses extends React.Component {
   tiltRef = React.createRef()
   componentDidMount() {
     const tiltNode = this.tiltRef.current
@@ -33,9 +56,14 @@ class Tilt extends React.Component {
 }
 function App() {
   return (
-    <Tilt>
-      <div className="totally-centered">vanilla-tilt.js</div>
-    </Tilt>
+    <>
+      <Tilt>
+        <div className="totally-centered">vanilla-tilt.js hooks</div>
+      </Tilt>
+      <TiltClasses>
+        <div className="totally-centered">vanilla-tilt.js classes</div>
+      </TiltClasses>
+    </>
   )
 }
 
